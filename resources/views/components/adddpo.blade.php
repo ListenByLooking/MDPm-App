4eb66396eda9dfda3a87497ff9bd2103d8326182
Giulio Pitteri <giulio.pitteri@phd.unipd.it>
2025-09-10 12:13:10 +0200

    Added missing files

---
+++ resources/views/components/adddpo.blade.php
@@ -0,0 +1,49 @@
+<div id="dpo_modal" class="modal fade flip" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <form method="POST" action="{{ route('dpo.insert') }}" id="score_form">
+            <input type="hidden" name="artwork_id" id="artwork_id" value="{{ $id }}">
+            @csrf
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="dpoModalLabel">DPO</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12 bg-success-subtle">
+                            <div class="form-group mb-2">
+                                <label for="dpo_year" class="form-label">Year</label>
+                                <input type="number" min="1901" step="1" placeholder="1970" id="dpo_year" class="form-control" name="dpo_year" required />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 bg-success-subtle">
+                            <div class="form-group mb-2">
+                                <label for="dpo_venue" class="form-label">Venue</label>
+                                <input type="text" id="dpo_venue" placeholder="Teatro Comunale G. Verdi" class="form-control" name="dpo_venue" required />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 bg-success-subtle">
+                            <div class="form-group mb-2">
+                                <label for="dpo_city" class="form-label">City</label>
+                                <input type="text" id="dpo_city" placeholder="Padova" class="form-control" name="dpo_city" required />
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer border border-top">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary ">Save Changes</button>
+            </div>
+        </div><!-- /.modal-content -->
+        </form>
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
---
+++ resources/views/components/hardware.blade.php
@@ -0,0 +1,93 @@
+<div class="col-12 components_div_right" id="hardware">
+    <h2 class="text-center">Hardware Form</h2>
+    <input type="hidden" name="form_name" value="phonographicdisks">
+    <input type="hidden" name="artwork_id" value="{{$id}}">
+    <div class="row">
+
+        <div class="col-4">
+            <div class="form-group">
+                <label for="preservation_signature">Preservation Signature</label>
+                <input type="text" id="preservation_signature" name="preservation_signature" required class="form-control" />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" required class="form-control" />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="manufacturer">Manufacturer</label>
+                <div class="input-group">
+                    <select id="manufacturer" name="manufacturer" required="" class="form-control">
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="C">C</option>
+                    </select>
+                    <button type="button" class="add-button btn btn-success" onclick="dpo.addOption('phone_brand')">+</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="model">Model</label>
+                <input type="text" id="model" name="model" required class="form-control" />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="os">Serial Number</label>
+                <input type="text" id="serial" name="serial" required class="form-control" />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="os">Operating System</label>
+                <input type="text" id="os" name="os" required class="form-control" />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="year">Year</label>
+                <input type="number" min="1901" step="1" placeholder="1970" id="year" class="form-control" name="year" required />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="cpu">CPU</label>
+                <input type="text" id="cpu" class="form-control" name="cpu" required />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="ram">RAM</label>
+                <input type="text" id="ram" class="form-control" name="ram" required />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="storage">Storage</label>
+                <input type="text" id="storage" class="form-control" name="storage" required />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="description">Description</label>
+                <input type="text" id="description" class="form-control" name="description" required />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="display">Display</label>
+                <input type="text" id="display" name="display" required class="form-control" />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="notes">Notes</label>
+                <textarea id="notes" name="notes" rows="3" class="form-control"></textarea>
+            </div>
+        </div>
+    </div>
+</div>
